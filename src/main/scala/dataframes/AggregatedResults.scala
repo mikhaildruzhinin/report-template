a02d46e01e0002dcddf58e9c01ecf557ff28f631
Mikhail Druzhinin <druzhininmy@gmail.com>
2022-05-20 17:51:52 +0300

    refactor aggregate results class

--- src/main/scala/dataframes/AggregatedResults.scala
+++ src/main/scala/dataframes/AggregatedResults.scala
@@ -5,36 +5,16 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, round, sum}
 
 class AggregatedResults(spark: SparkSession, databaseUrl: String, products: Products, sales: Sales, aggregateByColumns: Map[String, Boolean]) extends BaseDataFrame {
+  private val columnsToGroupBy: Array[String] = aggregateByColumns.filter(_._2).keySet.toArray
+
   val df: DataFrame = {
-    val columnsToGroupBy = aggregateByColumns.filter(_._2).keySet.toArray
     val groupByRegion = aggregateByColumns.getOrElse("region", false)
 
     val salesMatchedWithBrands = matchSalesAndBrands(products.df, sales.df)
     val salesMatchedByRegion = matchSalesAndRegions(salesMatchedWithBrands, groupByRegion)
 
-    salesMatchedByRegion.groupBy(columnsToGroupBy.head, columnsToGroupBy.tail: _*)
-      .agg(
-        round(
-          sum("total_sum"),
-          2
-        ).as("total_sum"))
-      .orderBy(columnsToGroupBy.head, columnsToGroupBy.tail: _*)
-      .withColumn(
-        "total_sum_pct",
-        round(
-          col("total_sum") / sum("total_sum").over(
-            if (columnsToGroupBy.length > 1) {
-              Window.partitionBy(
-                columnsToGroupBy.dropRight(1).head,
-                columnsToGroupBy.dropRight(1).tail: _*
-              )
-            } else {
-              Window.partitionBy() // can't get the head of an empty array
-            }
-          ),
-          2
-        )
-      )
+    val totalSum = calculateTotalSum(salesMatchedByRegion)
+    calculateTotalSumPct(totalSum)
   }
 
   private def matchSalesAndBrands(products: DataFrame, sales: DataFrame): DataFrame = {
@@ -58,5 +38,34 @@ class AggregatedResults(spark: SparkSession, databaseUrl: String, products: Prod
     } else {
       sales
     }
+  }
+
+  private def calculateTotalSum(sales: DataFrame): DataFrame = {
+    sales.groupBy(columnsToGroupBy.head, columnsToGroupBy.tail: _*)
+      .agg(
+        round(
+          sum("total_sum"),
+          2
+        ).as("total_sum"))
+      .orderBy(columnsToGroupBy.head, columnsToGroupBy.tail: _*)
+  }
+
+  private def calculateTotalSumPct(totalSum: DataFrame): DataFrame = {
+    totalSum.withColumn(
+      "total_sum_pct",
+      round(
+        col("total_sum") / sum("total_sum").over(
+          if (columnsToGroupBy.length > 1) {
+            Window.partitionBy(
+              columnsToGroupBy.dropRight(1).head,
+              columnsToGroupBy.dropRight(1).tail: _*
+            )
+          } else {
+            Window.partitionBy() // can't get the head of an empty array
+          }
+        ),
+        2
+      )
+    )
   }
 }
